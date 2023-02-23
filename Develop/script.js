ecfe3ed7bc13a4c65728351b4141b17f8a3b88d9
JoelCupeles <joel.cupeles1998@gmail.com>
2023-02-23 15:41:41 -0600

    Password Validation

--- Develop/script.js
+++ Develop/script.js
@@ -19,4 +19,38 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
   var length = prompt("Choose a password length between 8 and 128 characters");
   length = parseInt(length);
+
+
+//Validate the password lenth
+
+ if(isNaN(length) || length < 8 || length > 128) {
+  alert('Invdalid password length. Please choose a length between 8 and 128');
+
+ return '';
+ }
+
+ var includeLowercase = confirm("Do you want to include lowercase letters?");
+ var includeUppercase = confirm("Do you want to include Uppercase letters?");
+ var includeNumeric = confirm("Do you want to include numbers?");
+ var includeSpecial = confirm("Do you want to include special charcteres letters?")
+
+ if(!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+  alert("Please select at least one character type to include.");
+  return "";
+ }
+
+ //Charcter types 
+ var includeLowercase = "abcdefghijklmnopqrstuvwxy";
+ var includeUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ var includeNumeric = "0123456789";
+ var includeSpecial = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}"
+
+
+
+ 
 }
+
+ 
+
+
+
