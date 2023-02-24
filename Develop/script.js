39d910ccab8750a5094cab7415dfe9173303a43f
JoelCupeles <joel.cupeles1998@gmail.com>
2023-02-23 18:14:56 -0600

    Generate password

--- Develop/script.js
+++ Develop/script.js
@@ -1,56 +1,59 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-// Function to generate password//
-
+// Function to generate password
 function generatePassword() {
   var length = prompt("Choose a password length between 8 and 128 characters");
   length = parseInt(length);
 
+  // Validate the password length
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Invalid password length. Please choose a length between 8 and 128.");
+    return "";
+  }
 
-//Validate the password lenth
+  var includeLowercase = confirm("Do you want to include lowercase letters?");
+  var includeUppercase = confirm("Do you want to include uppercase letters?");
+  var includeNumeric = confirm("Do you want to include numbers?");
+  var includeSpecial = confirm("Do you want to include special characters?");
 
- if(isNaN(length) || length < 8 || length > 128) {
-  alert('Invdalid password length. Please choose a length between 8 and 128');
+  // Validate the character types selection
+  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+    alert("Please select at least one character type to include.");
+    return "";
+  }
 
- return '';
- }
+  // Define all character types
+  var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
- var includeLowercase = confirm("Do you want to include lowercase letters?");
- var includeUppercase = confirm("Do you want to include Uppercase letters?");
- var includeNumeric = confirm("Do you want to include numbers?");
- var includeSpecial = confirm("Do you want to include special charcteres letters?")
+  // Define the possible character types based on user selection
+  var possibleCharacters = "";
+  if (includeLowercase) possibleCharacters += lowercaseLetters;
+  if (includeUppercase) possibleCharacters += uppercaseLetters;
+  if (includeNumeric) possibleCharacters += numbers;
+  if (includeSpecial) possibleCharacters += specialCharacters;
 
- if(!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
-  alert("Please select at least one character type to include.");
-  return "";
- }
+  // Generate the password
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * possibleCharacters.length);
+    password += possibleCharacters.charAt(randomIndex);
+  }
 
- //Charcter types 
- var includeLowercase = "abcdefghijklmnopqrstuvwxy";
- var includeUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
- var includeNumeric = "0123456789";
- var includeSpecial = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}"
-
-
-
- 
+  return password;
 }
-
- 
-
-
 
